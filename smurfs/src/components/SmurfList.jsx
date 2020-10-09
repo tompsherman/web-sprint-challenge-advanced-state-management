52b3771ccd206027ab5cf70da72ec6c95daf3c31
unknown <tompsherman@gmail.com>
2020-10-09 11:47:11 -0400

    smurf list connected

--- smurfs/src/components/SmurfList.jsx
+++ smurfs/src/components/SmurfList.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+import {connect} from "react-redux"
+import {fetchSmurfs} from "../actions"
 
 import SmurfCard from "./SmurfCard.jsx"
 
 
-const SmurfList = () => {
+const SmurfList = (props) => {
+
+    useEffect(()=>{
+        props.fetchSmurfs()
+    }, [])
     return (
         <div>
             <h2>this is the smurf list</h2>
-            <SmurfCard />
+            x{
+                props.smurfs.map(item => (
+                    <SmurfCard key={item.id} item={item} />
+                ))
+            }
         </div>
     )
 }
 
-export default SmurfList
+const mapStateToProps = (state) => {
+    return {
+        smurfs: state.smurfs,
+        isLoading: state.isLoading,
+        errors: state.errors
+    }
+}
+
+export default connect (mapStateToProps, {fetchSmurfs}) (SmurfList)
